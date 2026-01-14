5438c5ce29b6800702d65262426484f174b73c2e
DEVRAJ <193325790+imdevraj069@users.noreply.github.com>
2026-01-14 18:26:28 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -69,15 +69,15 @@ app.use((req, res, next) => {
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------
 const limit = process.env.LIMIT || "50mb";
 
-const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 30, // Limit each IP to 100 requests per windowMs
-  standardHeaders: true, 
-  legacyHeaders: false,
-  message: 'Too many requests, please try again later.',
-});
+// const limiter = rateLimit({
+//   windowMs: 5 * 60 * 1000, // 15 minutes
+//   max: 30, // Limit each IP to 100 requests per windowMs
+//   standardHeaders: true, 
+//   legacyHeaders: false,
+//   message: 'Too many requests, please try again later.',
+// });
 
-app.use(limiter);
+// app.use(limiter);
 
 // Parses incoming JSON payloads.
 app.use(
