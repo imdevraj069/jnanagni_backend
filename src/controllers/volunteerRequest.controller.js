aaa35959d1aa96c103b9d2418ae380bd6588bf6d
DEVRAJ <193325790+imdevraj069@users.noreply.github.com>
2026-01-14 16:24:30 +0530

    test

--- src/controllers/volunteerRequest.controller.js
+++ src/controllers/volunteerRequest.controller.js
@@ -42,7 +42,8 @@ const assignRoleAndNotify = async (userId, role, contextName, contextType) => {
 // 1. Student submits volunteer request
 export const submitVolunteerRequest = asyncHandler(async (req, res) => {
   let formData = req.body;
-  const userId = req.user.id;
+  console.log("Volunteer Request Form Data:", formData);
+  const userId = req.user._id;
   const eventId = await req.params.eventId;
 
   if (!eventId) {
